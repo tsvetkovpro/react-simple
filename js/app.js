186e9d36c6dc2e61702f3096be90cccc6b347960
tsvetkovpro <285019@gmail.com>
2016-10-19 10:50:15 +0500

    Использование state Cсылка "подробнее", по клику на которую - текст новости целиком.
    Cвойства (this.props) следует использовать только для чтения, а для динамических свойств нужно использовать так называемое "состояние" (state).
    
    getInitialState
     - Если вы определяете какое-то изменяемое свойство в компоненте, необходимо указать начальное состояние (в терминологии react.js - initial state). Для этого, у компонента указывается метод getInitialState
    
    Для сохранения динамических свойств, используется состояние (state) компонента.
    
    Для обработки клика, используется свойство onClick + функция-обработчик. Существуют и другие стандартные события, которые работают по такому же принципу. Полный список здесь, а мы будем знакомиться с ними по мере необходимости.
    
    Для изменения состояния, используется метод setState, который принимает объект с аргументами, которые нужно изменить. Например, у нас есть состояние:
    
    ...
    getInitialState: function() {
        return {
          visible: false,
          rating: 0,
          eshe_odno_svoistvo: 'qweqwe'
        };
      }
    ...
    
    Чтобы изменить рейтинг, нужно написать следующий setState:
    
    this.setState({rating: 100500})
    
    Чтобы изменить все три свойства:
    
    this.setState({
        rating: 100500,
        visible: true,
        eshe_odno_svoistvo: 'привет'
    })
    
    Так же у setState есть возможность указать callback функцию, которая будет вызвана после того, как новое состояние "установится".
    
    ...
    readmoreClick: function(e) {
        e.preventDefault();
        this.setState({visible: true}, function() {
          alert('Состояние изменилось');
        });
      },
    ...

--- js/app.js
+++ js/app.js
@@ -1,41 +1,65 @@
 var my_news = [
 	{
 		author: 'Саша Печкин',
-		text: 'В четчерг, четвертого числа...'
+		text: 'В четчерг, четвертого числа...',
+		bigText: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur, nisi!'
 	},
 	{
 		author: 'Вася Пупкин',
-		text: 'Считаю, что $ должен стоить 35 рублей!'
+		text: 'Считаю, что $ должен стоить 35 рублей!',
+		bigText: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur, nisi!'
 	},
 	{
 		author: 'Иванов Иван',
-		text: 'Бесплатно. Скачать. Лучший сайт - http://localhost:3001'
+		text: 'Бесплатно. Скачать. Лучший сайт - http://localhost:3001',
+		bigText: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur, nisi!'
 	},
 	{
 		author: 'Петров Петя',
-		text: 'Lorem ipsum dolor sit amet, consectetur.'
+		text: 'Lorem ipsum dolor sit amet, consectetur.',
+		bigText: 'Lorem ipsum dolor sit amet'
 	}
 ];
+
 
 var Article = React.createClass({
 	propTypes: {
 		data: React.PropTypes.shape({
 			author: React.PropTypes.string.isRequired,
-			text: React.PropTypes.string.isRequired
+			text: React.PropTypes.string.isRequired,
+			bigText: React.PropTypes.string.isRequired
 		})
+	},
+	getInitialState: function() {
+		return {
+			visible: false
+		};
+	},
+	readmoreClick: function(e) {
+		e.preventDefault();
+		this.setState({visible: true});
 	},
 	render: function() {
 		var author = this.props.data.author,
-			text = this.props.data.text;
+			text = this.props.data.text,
+			bigText = this.props.data.bigText,
+			visible = this.state.visible;
 
 		return (
 			<div className='article'>
 				<p className='news__author'>{author}:</p>
 				<p className='news__text'>{text}</p>
+				<a href="#"
+					 onClick={this.readmoreClick}
+					 className={'news__readmore ' + (visible ? 'none': '')}>
+					Подробнее
+				</a>
+				<p className={'news__big-text ' + (visible ? '': 'none')}>{bigText}</p>
 			</div>
 		)
 	}
 });
+
 
 var News = React.createClass({
 	propTypes: {
@@ -65,6 +89,7 @@ var News = React.createClass({
 		);
 	}
 });
+
 
 var App = React.createClass({
 	render: function() {
